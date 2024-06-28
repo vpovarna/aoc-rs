8e40478dedd98b00a32a0be4f9147edae3b4e0bc
povarna <povarna@adobe.com>
2024-06-28 19:10:00 +0300

    AoC2015 Scaffolding

---
+++ aoc2015/src/day04.rs
@@ -0,0 +1,16 @@
+fn part1(input_file_path: &str) -> u32 {
+    todo!()
+}
+
+fn part2(input_file_path: &str) -> u32 {
+    todo!()
+}
+
+
+
+pub fn run() {
+    let input_file_path = "input/2015/day04.txt";
+
+    println!("AoC2015, Day4, part1 solution is: {}", part1(input_file_path));
+    println!("AoC2015, Day4, part2 solution is: {}", part2(input_file_path));
+}
--- aoc2015/src/main.rs
+++ aoc2015/src/main.rs
@@ -1,10 +1,12 @@
 mod day01;
 mod day02;
 mod day03;
+mod day04;
 
 #[warn(unused_imports)]
 fn main() {
     day01::run();
     day02::run();
     day03::run();
+    day04::run();
 }
