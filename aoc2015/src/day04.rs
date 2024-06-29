6f49978608597aaaf133018c5ed71b73d218b066
povarna <povarna@adobe.com>
2024-06-29 19:05:22 +0300

    AoC2015 Day04 part1 and part2 solution

--- aoc2015/src/day04.rs
+++ aoc2015/src/day04.rs
@@ -1,10 +1,43 @@
+use aoclib::read_as_string;
+
 fn part1(input_file_path: &str) -> u32 {
-    todo!()
+    let input = read_as_string(input_file_path);
+
+    if let Some(value) = get_index(input, "00000") {
+        return value;
+    }
+    return 0
 }
 
 fn part2(input_file_path: &str) -> u32 {
-    todo!()
+        let input = read_as_string(input_file_path);
+
+    if let Some(value) = get_index(input, "000000") {
+        return value;
+    }
+    return 0
 }
+
+fn get_index(input: String, pattern: &str) -> Option<u32> {
+    let mut i = 100000;
+
+    loop {
+        let tmp_input = format!("{}{}", input, i);
+        let mut md5_hash = calculate_md5_hash(tmp_input);
+        if md5_hash.starts_with(pattern) {
+            return Some(i);
+        }
+        i += 1;
+    }
+    None
+}
+
+fn calculate_md5_hash(input: String) -> String {
+    let md5_hash_raw = md5::compute(input);
+    let md5_hash = format!("{:x}", md5_hash_raw);
+    md5_hash
+}
+
 
 
 
--- aoclib/src/lib.rs
+++ aoclib/src/lib.rs
@@ -4,10 +4,7 @@ use std::path::Path;
 
 
 pub fn read_to_chars(file_path_name: &str) -> Vec<char> {
-    let data = read_to_string(file_path_name).expect("unable to open file");
-
-    let chars: Vec<char> = data.chars().collect();
-    chars
+    read_as_string(file_path_name).chars().collect()
 }
 
 pub fn read_lines(file_path_name: impl AsRef<Path>) -> Vec<String> {
@@ -16,4 +13,8 @@ pub fn read_lines(file_path_name: impl AsRef<Path>) -> Vec<String> {
     buf.lines()
         .map(|line| line.expect("Could not parse line"))
         .collect()
+}
+
+pub fn read_as_string(file_path_name: &str) -> String {
+    return read_to_string(file_path_name).expect("unable to open file");
 }
