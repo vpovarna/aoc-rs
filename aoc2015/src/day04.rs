d283c6eeada1955043e8ae942c471f5e64f25986
povarna <povarna@adobe.com>
2024-06-29 19:06:56 +0300

    @trivial remove new lines

--- aoc2015/src/day04.rs
+++ aoc2015/src/day04.rs
@@ -6,16 +6,16 @@ fn part1(input_file_path: &str) -> u32 {
     if let Some(value) = get_index(input, "00000") {
         return value;
     }
-    return 0
+    return 0;
 }
 
 fn part2(input_file_path: &str) -> u32 {
-        let input = read_as_string(input_file_path);
+    let input = read_as_string(input_file_path);
 
     if let Some(value) = get_index(input, "000000") {
         return value;
     }
-    return 0
+    return 0;
 }
 
 fn get_index(input: String, pattern: &str) -> Option<u32> {
@@ -36,9 +36,6 @@ fn calculate_md5_hash(input: String) -> String {
     let md5_hash = format!("{:x}", md5_hash_raw);
     md5_hash
 }
-
-
-
 
 pub fn run() {
     let input_file_path = "input/2015/day04.txt";
