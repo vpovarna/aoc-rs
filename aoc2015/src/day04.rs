fa8e4d5083ec2dfbd37a99fd6a31571d829886f7
povarna <povarna@adobe.com>
2024-06-30 18:40:59 +0300

    @trivial refactor day4

--- aoc2015/src/day04.rs
+++ aoc2015/src/day04.rs
@@ -1,20 +1,18 @@
 use md5::compute;
 use aoclib::read_as_string;
 
-fn part1(input_file_path: &str) -> usize {
-    let input = read_as_string(input_file_path);
-    get_lowest_number(input, 5)
+fn part1(input: &String, starting_number: usize) -> usize {
+    get_lowest_number(input, 5, starting_number)
 }
 
-fn part2(input_file_path: &str) -> usize {
-    let input = read_as_string(input_file_path);
-    get_lowest_number(input, 6)
+fn part2(input: &String, starting_number: usize) -> usize {
+    get_lowest_number(input, 6, starting_number)
 }
 
 
-fn get_lowest_number(key: String, leading_zeros: usize) -> usize {
+fn get_lowest_number(key: &String, leading_zeros: usize, starting_number: usize) -> usize {
     let pattern = "0".repeat(leading_zeros);
-    (100000..).find(|n| {
+    (starting_number..).find(|n| {
         let md5_hash = compute(format!("{}{}", key, n));
         let md5 = format!("{:x}", md5_hash);
         md5.starts_with(&pattern)
@@ -22,8 +20,8 @@ fn get_lowest_number(key: String, leading_zeros: usize) -> usize {
 }
 
 pub fn run() {
-    let input_file_path = "input/2015/day04.txt";
-
-    println!("AoC2015, Day4, part1 solution is: {}", part1(input_file_path));
-    println!("AoC2015, Day4, part2 solution is: {}", part2(input_file_path));
+    let input = read_as_string( "input/2015/day04.txt");
+    let part1_solution= part1(&input, 0);
+    println!("AoC2015, Day4, part1 solution is: {}", part1_solution );
+    println!("AoC2015, Day4, part2 solution is: {}", part2(&input, part1_solution));
 }
