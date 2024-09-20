4c8a684dc6d520691dce4a5020e2e4ddcd80c9ca
povarna <povarna@adobe.com>
2024-09-20 11:41:27 +0300

    AoC2015 Day14 Part1 solution

---
+++ aoc2015/src/day14.rs
@@ -0,0 +1,51 @@
+use itertools::Itertools;
+use aoclib::read_lines;
+
+#[derive(Clone)]
+struct Reindeer {
+    _name: String,
+    speed: u32,
+    duration: u32,
+    rest: u32,
+}
+
+impl Reindeer {
+    fn distance(&self, seconds: u32) -> u32 {
+        let stride = self.duration + self.rest;
+        let stride_count = seconds / stride;
+        let remainder = seconds % stride;
+
+        stride_count * self.speed * self.duration + self.speed * remainder.min(self.duration)
+    }
+}
+
+pub fn run() {
+    let lines = read_lines("input/2015/day14.txt");
+    let stats = parse_input(lines);
+
+    println!("Part 1: {}", part1(stats.clone(), 2503));
+    println!("Part 2: {}", part2(stats, 2503));
+}
+
+fn parse_input(lines: Vec<String>) -> Vec<Reindeer> {
+    lines.iter()
+        .map(|line| {
+            let parts = line.split(" ").collect_vec();
+            Reindeer {
+                _name: parts[0].to_string(),
+                speed: parts[3].parse().unwrap(),
+                duration: parts[6].parse().unwrap(),
+                rest: parts[13].parse().unwrap(),
+            }
+        })
+        .collect_vec()
+}
+
+
+fn part1(reindeer_stats: Vec<Reindeer>, end_time: u32) -> u32 {
+    return  reindeer_stats.iter().map(|d| d.distance(end_time)).max().unwrap();
+}
+
+fn part2(reindeer_stats: Vec<Reindeer>, end_time: u32) -> u32 {
+    return 1;
+}
--- aoc2015/src/main.rs
+++ aoc2015/src/main.rs
@@ -11,6 +11,7 @@ mod day10;
 mod day11;
 mod day12;
 mod day13;
+mod day14;
 
 #[warn(unused_imports)]
 fn main() {
@@ -26,5 +27,6 @@ fn main() {
     // day10::run();
     // day11::run();
     // day12::run();
-     day13::run()
+    //  day13::run();
+    day14::run();
 }
