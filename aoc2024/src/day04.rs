d18db3e59fb152e78085534a84c23f7ea71e7ffe
povarna <povarna@adobe.com>
2024-12-03 21:35:12 +0200

    @trivial AOC2024 day04 scaffolding.

---
+++ aoc2024/src/day04.rs
@@ -0,0 +1,16 @@
+use aoclib::read_as_string;
+
+#[allow(dead_code)]
+pub fn run() {
+    let input = read_as_string("input/2024/day04.txt");
+    println!("Part1: {}", part1(&input));
+    println!("Part2: {}", part2(&input));
+}
+
+fn part1(input: &String) -> usize {
+    1
+}
+
+fn part2(input: &String) -> usize {
+    1
+}
