12d94a91aa41e2ecdf2a7262bb1f938fc02a30c7
povarna <povarna@adobe.com>
2024-12-03 21:33:39 +0200

    @trivial AOC2024 day03 part2 solution

--- aoc2024/src/day03.rs
+++ aoc2024/src/day03.rs
@@ -1,18 +1,15 @@
 use regex::Regex;
 use aoclib::read_as_string;
 
-static MULE_REGEX: Regex = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
-
 #[allow(dead_code)]
 pub fn run() {
-    println!("Part1: {}", part1());
-    println!("Part1: {}", part2());
+    let input = read_as_string("input/2024/day03.txt");
+    println!("Part1: {}", part1(&input));
+    println!("Part2: {}", part2(&input));
 }
 
 #[allow(dead_code)]
-fn part1() -> u32 {
-    let input = read_as_string("input/2024/day03.txt");
-
+fn part1(input: &String) -> u32 {
     // Define the regex pattern
     let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
 
@@ -26,6 +23,37 @@ fn part1() -> u32 {
 }
 
 
+#[allow(dead_code)]
+fn part2(input: &String) -> u32 {
+    // Regex patterns
+    let mul_re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+    let do_re = Regex::new(r"do\(\)").unwrap();
+    let dont_re = Regex::new(r"don't\(\)").unwrap();
+
+    // State tracking
+    let mut mul_enabled = true;
+    let mut total = 0;
+
+    // Use regex to extract meaningful tokens
+    let token_re = Regex::new(r"do\(\)|don't\(\)|mul\((\d+),(\d+)\)").unwrap();
+
+    for cap in token_re.captures_iter(&input) {
+        let token = &cap[0]; // The matched token as a whole
+
+        if do_re.is_match(token) {
+            mul_enabled = true;
+        } else if dont_re.is_match(token) {
+            mul_enabled = false;
+        } else if mul_enabled && mul_re.is_match(token) {
+            total += calculate_product(token);
+        }
+    }
+
+    total
+}
+
+
+#[allow(dead_code)]
 fn calculate_product(mul: &str) -> u32 {
     let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
 
@@ -36,9 +64,4 @@ fn calculate_product(mul: &str) -> u32 {
     } else {
         1
     }
-}
-
-#[allow(dead_code)]
-fn part2() -> i32 {
-    0
 }
