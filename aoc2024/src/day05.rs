b2451c1e5392d324b2b2dd34033482aaa91f376a
povarna <povarna@adobe.com>
2024-12-05 09:41:03 +0200

    @trivial AOC2024 day05 part1 solution

---
+++ aoc2024/src/day05.rs
@@ -0,0 +1,63 @@
+use std::collections::HashMap;
+use itertools::Itertools;
+use aoclib::read_as_string;
+
+pub fn run() {
+    let lines = read_as_string("input/2024/day05.txt");
+
+    println!("Part1: {}", part1(&lines));
+    println!("Part2: {}", part2(&lines));
+}
+
+fn part1(input: &String) -> usize {
+    let (rules, updates) = parse_input(input);
+
+    let mut result = 0;
+    for updates in updates {
+        let (is_good, mid) = follows_rules(&rules, &updates);
+        if is_good {
+            result += mid;
+        }
+    }
+    result
+}
+
+fn follows_rules(rules: &Vec<(u16, u16)>, updates: &Vec<u16>) -> (bool, usize) {
+    let mut idx_map: HashMap<u16, u16> = HashMap::new();
+
+    for (i, num) in updates.iter().enumerate() {
+        idx_map.insert(*num, i as u16);
+    }
+
+    for (a, b) in rules {
+        if idx_map.contains_key(a) && idx_map.contains_key(b) && !(idx_map[a] < idx_map[b]) {
+            return (false, 0);
+        }
+    }
+
+    (true, updates[updates.len() / 2] as usize)
+}
+
+fn part2(input: &String) -> usize {
+    2
+}
+
+
+fn parse_input(input: &String) -> (Vec<(u16, u16)>, Vec<Vec<u16>>) {
+    let (parts1, part2) = input.split_once("\n\n").unwrap();
+
+    let mut rules: Vec<(u16, u16)> = Vec::new();
+
+    for line in parts1.lines() {
+        let v = line.split("|").collect_vec();
+        rules.push((v[0].parse().unwrap(), v[1].parse().unwrap()));
+    }
+
+    let mut updates: Vec<Vec<u16>> = Vec::new();
+    for line in part2.lines() {
+        let tmp_vec = line.split(",").map(|x| x.parse::<u16>().unwrap()).collect_vec();
+        updates.push(tmp_vec);
+    }
+
+    (rules, updates)
+}
